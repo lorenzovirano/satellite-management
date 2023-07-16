14634ebce3f33192c16d9143cb5200927c55aca4
vgu <vgu@bit4id.com>
2023-07-16 20:53:26 +0200

    rush procedures

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -30,7 +30,12 @@ BEGIN
     ) THEN
         SIGNAL SQLSTATE '46001' SET MESSAGE_TEXT = "Il satellite non esiste.";
     ELSE
-        SELECT 1 FROM satellite WHERE nome = p_nome_satellite;
+        SELECT satellite.nome , coordinate_orbitali.altitudine, coordinate_orbitali.inclinazione, coordinate_orbitali.eccentricità, GROUP_CONCAT(strumento_di_bordo_satellite.strumento_di_bordo_nome SEPARATOR ', ')
+        FROM satellite
+        JOIN coordinate_orbitali ON satellite.nome = coordinate_orbitali.satellite_nome
+        JOIN strumento_di_bordo_satellite ON satellite.nome = strumenti_di_bordo_satellite.satellite_nome
+        WHERE satellite.nome = p_nome_satellite
+        GROUP BY satellite.nome;
     END IF;
 END //
 
@@ -40,11 +45,9 @@ CREATE PROCEDURE GetCrews(
 )
 BEGIN
     IF NOT EXISTS (
-        SELECT crew.* FROM stazione_terrestre 
-        JOIN crew ON crew.stazione_terrestre_nome = stazione_terrestre.nome
-        WHERE stazione_terrestre.operatore_nome = p_nome_operatore
+        SELECT 1 FROM operatore WHERE nome = p_nome_operatore
     ) THEN
-        SIGNAL SQLSTATE '46002' SET MESSAGE_TEXT = "L'operatore non ha Crew";
+        SIGNAL SQLSTATE '46002' SET MESSAGE_TEXT = "L'operatore non esiste";
     ELSE
         SELECT crew.* FROM stazione_terrestre 
         JOIN crew ON crew.stazione_terrestre_nome = stazione_terrestre.nome
@@ -58,12 +61,9 @@ CREATE PROCEDURE GetMissioniByNomeOperatore(
 )
 BEGIN
     IF NOT EXISTS (
-        SELECT missione.*
-        FROM satellite
-        JOIN missione ON missione.satellite_nome = satellite.nome
-        WHERE satellite.operatore_nome = p_nome_operatore
+        SELECT 1 FROM operatore WHERE nome = p_nome_operatore
     ) THEN
-        SIGNAL SQLSTATE '46004' SET MESSAGE_TEXT = "L'operatore non ha missioni associate";
+        SIGNAL SQLSTATE '46003' SET MESSAGE_TEXT = "L'operatore non esiste";
     ELSE
         SELECT missione.*
         FROM satellite
@@ -78,9 +78,9 @@ CREATE PROCEDURE GetRilevazioniBySatellite(
 )
 BEGIN
     IF NOT EXISTS (
-        SELECT 1  FROM rilevazioe WHERE satellite_nome = p_nome_satellite
+        SELECT 1 FROM satellite WHERE nome = p_nome_satellite
     ) THEN
-        SIGNAL SQLSTATE '46005' SET MESSAGE_TEXT = "Il satellite non ha rilevazioni";
+        SIGNAL SQLSTATE '46004' SET MESSAGE_TEXT = "Il satellite non esiste.";
     ELSE
         SELECT *  FROM rilevazioe WHERE satellite_nome = p_nome_satellite;
     END IF;
@@ -92,9 +92,9 @@ CREATE PROCEDURE GetGuasti(
 )
 BEGIN
     IF NOT EXISTS (
-        SELECT 1  FROM guasto WHERE satellite_nome = p_nome_satellite
+        SELECT 1 FROM satellite WHERE nome = p_nome_satellite
     ) THEN
-        SIGNAL SQLSTATE '46006' SET MESSAGE_TEXT = "Il satellite non è stato afflitto da guasti";
+        SIGNAL SQLSTATE '46005' SET MESSAGE_TEXT = "Il satellite non esiste.";
     ELSE
         SELECT *  FROM guasto WHERE satellite_nome = p_nome_satellite;
     END IF;
@@ -105,12 +105,46 @@ CREATE PROCEDURE GetGuastiByStrumentoNome(
 )
 BEGIN
     IF NOT EXISTS (
-        SELECT 1  FROM rilevazioe WHERE strumento_di_bordo_nome = p_nome_strumento
+        SELECT 1  FROM strumento_di_bordo WHERE nome = p_nome_strumento
     ) THEN
-        SIGNAL SQLSTATE '46005' SET MESSAGE_TEXT = "Lo strumento di bordo non ha eseguito rilevazioni";
+        SIGNAL SQLSTATE '46006' SET MESSAGE_TEXT = "Lo strumento di bordo non esiste";
     ELSE
         SELECT *  FROM rilevazioe WHERE strumento_di_bordo_nome = p_nome_strumento;
     END IF;
 END //
+
+
+CREATE PROCEDURE InsertMissione(
+    IN p_nome VARCHAR(255),
+    IN p_data_inizio DATETIME,
+    IN p_tipo_missione VARCHAR(255),
+    IN p_crew_nome VARCHAR(255),
+    IN p_satellite_nome VARCHAR(255)
+)
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM tipo_missione
+        WHERE tipo = p_tipo_missione
+    ) THEN
+        SIGNAL SQLSTATE '46007' SET MESSAGE_TEXT = "Inserire un corretto tipo di missione.";
+    ELSEIF NOT EXISTS (
+        SELECT 1
+        FROM crew
+        WHERE nome = p_crew_nome
+    ) THEN
+        SIGNAL SQLSTATE '46008' SET MESSAGE_TEXT = "La crew inserita non esiste.";
+    ELSEIF NOT EXISTS (
+        SELECT 1
+        FROM satellite
+        WHERE nome = p_satellite_nome
+    ) THEN
+        SIGNAL SQLSTATE '46009' SET MESSAGE_TEXT = "Il satellite non esiste";
+    ELSE
+        INSERT INTO missione (nome, data_inizio, tipo_missione_tipo, crew_nome, satellite_nome)
+        VALUES  (p_nome, p_data_inizio, p_tipo_missione, p_crew_nome, p_satellite_nome);
+    END IF;
+END //
+
 
 DELIMITER ;
