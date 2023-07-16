fa666c77735ab26f1b1026df089dc0663364f089
vgu <vgu@bit4id.com>
2023-07-16 23:54:35 +0200

    add query creation user

---
+++ file da inviare/CreazioneUtenti.sql
@@ -0,0 +1,6 @@
+CREATE USER db_satellite_managment IDENTIFIED BY admin ;
+CREATE USER operatore IDENTIFIED BY 1234 ;
+CREATE USER membro_equipaggio IDENTIFIED BY abcd ;
+CREATE USER tecnico IDENTIFIED BY qwer ;
+CREATE USER ricercatore IDENTIFIED BY pass123 ;
+GRANT ALL PRIVILEGES TO db_satellite_managment ;
